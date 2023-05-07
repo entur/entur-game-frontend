3e0dc038a2127b3d2ca232244fcc68b05aa092f0
Kenneth Ngo <kenneth.ngo@entur.org>
2023-05-07 15:27:54 +0200

    upgrade packages and fix npm run build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin(), VitePWA()],
     server: {
         port: 3000,
         open: true,
