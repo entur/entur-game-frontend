2c2dcd5b6a3af717f6a68171a4528a0a7cfe6f3e
Daniel Jansson <daniel.jansson@entur.org>
2023-05-12 14:24:27 +0200

    changed how we send seconds into backend

--- src/utils/dateFnsUtils.ts
+++ src/utils/dateFnsUtils.ts
@@ -12,23 +12,19 @@ export function formatInterval(currentTime: Date, startTime: Date): string {
 }
 
 export function formatTimeForEndOfGame(currentTime: Date, startTime: Date): string{
-    const timePlayed = intervalToDuration({ end: currentTime, start: startTime })
-    const hours = (timePlayed.hours ?? 0 * 3600).toString()
-    const minutes = (timePlayed.minutes ?? 0 * 60).toString()
-    const seconds = (timePlayed.seconds ?? 0).toString()
-    return  hours + ' timer ' + minutes + ' minutter ' + seconds + ' sekunder'
+    const totalSecondsPlayed =  (currentTime.getTime() - startTime.getTime()) / 1000
+    const hours = Math.round(totalSecondsPlayed/3600)
+    const minutes = Math.round((totalSecondsPlayed % 3600) / 60)
+    const seconds = Math.round(totalSecondsPlayed % 60)
+    return hours.toString() + ' timer ' + minutes.toString() + ' minutter ' + seconds.toString() + ' sekunder'
 }
 
 export function formatIntervalToSeconds(
     currentTime: Date,
     startTime: Date,
 ): number {
-    const duration = intervalToDuration({ end: currentTime, start: startTime })
-    return (
-        (duration.seconds ?? 0) +
-        (duration.minutes ?? 0) * 60 +
-        (duration.hours ?? 0) * 3600
-    )
+    const totalSecondsPlayed =  Math.round((currentTime.getTime() - startTime.getTime()) / 1000)
+    return totalSecondsPlayed
 }
 
 export function formatTime(value: Date | string): string {
