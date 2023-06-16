d60e50db063a4ddad840551788057c15ff68e78d
Kenneth Ngo <kenneth.ngo@entur.org>
2023-06-16 12:11:10 +0200

    init textcolors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+import { colors } from '@entur/tokens'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        textColor: {
+            'blue-main': colors.brand.blue,
+            'blue-50': colors.blues.blue50,
+            'blue-80': colors.blues.blue80,
+        },
         extend: {},
     },
     plugins: [],
