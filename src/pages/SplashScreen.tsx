c3c7fb4294b3eb135cb6f2152095c1e29691207d
Kenneth Ngo <kenneth.ngo@entur.org>
2023-07-14 09:06:29 +0200

    change CSS global id to className to not conflict rest of CSS

--- src/pages/SplashScreen.tsx
+++ src/pages/SplashScreen.tsx
@@ -1,26 +1,39 @@
 import './SplashScreen.css'
+
+import React from 'react'
 import { Paragraph } from '@entur/typography'
 import { Button } from '@entur/button'
-import { useNavigate } from 'react-router-dom';
-export function SplashScreen(): JSX.Element {
-    const navigate = useNavigate();
-    return (
-        <>
-            <div id='illustration1' />
-            <div id='illustration2' />
-            <div id='illustration3' />
-            <div id='centertext'>
-                <Paragraph id='en'>EN</Paragraph>
-                <Paragraph id='middels'>MIDDELS</Paragraph>
-                <Paragraph id='tur'>TUR</Paragraph>
-            </div>
-            <div id='infotext'>
-                <Paragraph margin='none' id='welcometext'>Velkommen om bord!</Paragraph>
-                <Paragraph id='questiontext'>Er du smartere enn vår reiseplanlegger? Bli med på turen og vinn gratis kollektivreiser!</Paragraph>
-            </div>
-            <Button onClick={() => navigate("/")} variant={'success'}>Kjør!</Button>
+import { useNavigate } from 'react-router-dom'
 
-        </>
+export function SplashScreen(): JSX.Element {
+    const navigate = useNavigate()
+    return (
+        <div className="splashScreen">
+            <div className="illustration1" />
+            <div className="illustration2" />
+            <div className="illustration3" />
+            <div className="centertext">
+                <Paragraph className="en">EN</Paragraph>
+                <Paragraph className="middels">MIDDELS</Paragraph>
+                <Paragraph className="tur">TUR</Paragraph>
+            </div>
+            <div className="infotext">
+                <Paragraph margin="none" className="welcometext">
+                    Velkommen om bord!
+                </Paragraph>
+                <Paragraph className="questiontext">
+                    Er du smartere enn vår reiseplanlegger? Bli med på turen og
+                    vinn gratis kollektivreiser!
+                </Paragraph>
+            </div>
+            <Button
+                className="button"
+                onClick={() => navigate('/')}
+                variant={'success'}
+            >
+                Kjør!
+            </Button>
+        </div>
     )
 }
 
