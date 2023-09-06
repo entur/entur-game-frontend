e3f7a1a16f213310ed44aeb9867f650282e6df4f
Kenneth Ngo <kenneth.ngo@entur.org>
2023-09-06 08:31:53 +0200

    fix eventscore

--- src/pages/EventHighscorePage.tsx
+++ src/pages/EventHighscorePage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Heading1, Heading2, Heading3, Paragraph } from '@entur/typography'
+import React from 'react'
+import { Heading1, Heading3, Paragraph } from '@entur/typography'
 import {
     Table,
     TableHead,
@@ -9,126 +9,96 @@ import {
     HeaderCell,
 } from '@entur/table'
 import '@entur/table/dist/styles.css'
-import { PlayerResponse } from '../api/playerScoreApi'
+import { getByDifficulty } from '../api/playerScoreApi'
 import { generateKey } from '../utils/generateUniqueKey'
 import EnInsertTur from '../components/EnInsertTur'
-
-enum Difficulty {
-    Easy = 'Lett',
-    Medium = 'Middels',
-    Hard = 'Vanskelig',
-    Event = 'Event',
-}
+import { useFlags } from 'flagsmith/react'
+import useSWR from 'swr'
 
 export const EventHighscorePage = (): JSX.Element => {
-    const [players, setPlayers] = useState<PlayerResponse[]>([])
-    function getNorwegianWeekday() {
-        const today = new Date()
-        const weekdays = [
-            'Søndag',
-            'Mandag',
-            'Tirsdag',
-            'Onsdag',
-            'Torsdag',
-            'Fredag',
-            'Lørdag',
-        ]
-        const weekdayIndex = today.getDay()
-        return weekdays[weekdayIndex]
-    }
-    const difficulty = Difficulty.Event
-
-    function generateMockPlayers(count: number): PlayerResponse[] {
-        const players: PlayerResponse[] = []
-
-        for (let i = 0; i < count; i++) {
-            const player: PlayerResponse = {
-                name: `Player ${i + 1}`,
-                score: Math.floor(Math.random() * 100),
-                difficulty: 'Event',
-                totalOptions: Math.floor(Math.random() * 100),
-                totalTravelTime: Math.floor(Math.random() * 100),
-                totalPlaytime: Math.floor(Math.random() * 100),
-                fromDestination: {
-                    id: 'fromId',
-                    destination: 'fromDestination',
-                },
-                toDestination: { id: 'toId', destination: 'toDestination' },
-                email: 'player' + i + '@mock.com',
-                //set phone number to random 8 digit number
-                phoneNumber: Math.floor(Math.random() * 100000000),
-            }
-            //sort players by score
-
-            players.push(player)
-        }
-        players[0].name = 'a'.repeat(50)
-        players.sort((a, b) => b.score - a.score)
-
-        return players
-    }
-
-    useEffect(() => {
-        //     const fetchData = async () => {
-        //         const data = await getTopTenByDifficulty(difficulty)
-        //         setPlayers(data)
-        //     }
-        setPlayers(generateMockPlayers(100))
-        //sort players by score
-
-        //     fetchData()
-    }, [difficulty])
+    const { javazone2 } = useFlags(['javazone2'])
+    const difficulty = javazone2.enabled ? 'Javazone42' : 'Javazone1'
+    const { data: players } = useSWR(
+        '/players',
+        () => getByDifficulty(difficulty),
+        { refreshInterval: 1000 * 10 },
+    )
     if (players === undefined) {
         return <p>Laster inn...</p>
     }
 
-
     return (
-        <>
+        <div className="h-screen w-screen">
             <EnInsertTur />
-            <br />
-            <br />
-            <br />
-            <br />
-            <Table className='text-white'>
+            <Table className="text-white">
                 <TableHead>
                     <TableRow>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Rank</Heading1>
+                            <Heading1 className="text-white/25">Rank</Heading1>
                         </HeaderCell>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Navn</Heading1>
+                            <Heading1 className="text-white/25">Navn</Heading1>
                         </HeaderCell>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Poengsum</Heading1>
+                            <Heading1 className="text-white/25">
+                                Poengsum
+                            </Heading1>
                         </HeaderCell>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Spillertrekk</Heading1>
+                            <Heading1 className="text-white/25">
+                                Spillertrekk
+                            </Heading1>
                         </HeaderCell>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Total reisetid</Heading1>
+                            <Heading1 className="text-white/25">
+                                Total reisetid
+                            </Heading1>
                         </HeaderCell>
                         <HeaderCell>
-                            <Heading1 className='text-white/25'>Total spilletid</Heading1>
+                            <Heading1 className="text-white/25">
+                                Total spilletid
+                            </Heading1>
                         </HeaderCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {players.map((player, index) => (
-                        <TableRow
-                            key={generateKey(player.score + player.name)}
-                        >
-                            <DataCell><Heading1 className='text-white'>{index + 1}</Heading1></DataCell>
-                            <DataCell><Heading3 className='text-white'>{player.name}</Heading3></DataCell>
-                            <DataCell><Heading3 className='text-white'>{player.score}</Heading3></DataCell>
-                            <DataCell><Heading3 className='text-white'>{player.totalOptions}</Heading3></DataCell>
-                            <DataCell><Heading3 className='text-white'>{player.totalTravelTime}</Heading3></DataCell>
-                            <DataCell><Heading3 className='text-white'>{player.totalPlaytime}</Heading3></DataCell>
+                    {players.slice(0, 30).map((player, index) => (
+                        <TableRow key={generateKey(player.score + player.name)}>
+                            <DataCell>
+                                <Heading1 className="text-white">
+                                    {index + 1}
+                                </Heading1>
+                            </DataCell>
+                            <DataCell>
+                                <Heading3 className="text-white">
+                                    {player.name}
+                                </Heading3>
+                            </DataCell>
+                            <DataCell>
+                                <Heading3 className="text-white">
+                                    {player.score}
+                                </Heading3>
+                            </DataCell>
+                            <DataCell>
+                                <Heading3 className="text-white">
+                                    {player.totalOptions}
+                                </Heading3>
+                            </DataCell>
+                            <DataCell>
+                                <Heading3 className="text-white">
+                                    {player.totalTravelTime}
+                                </Heading3>
+                            </DataCell>
+                            <DataCell>
+                                <Heading3 className="text-white">
+                                    {player.totalPlaytime}
+                                </Heading3>
+                            </DataCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-        </>
+        </div>
     )
 }
 export default EventHighscorePage
