17505cda2a9d66dd2249d302dd4a08ff8ad5fc05
Kenneth Ngo <kenneth.ngo@entur.org>
2023-09-07 08:36:16 +0200

    javazone42

--- src/pages/EventHighscorePage.tsx
+++ src/pages/EventHighscorePage.tsx
@@ -20,7 +20,7 @@ export const EventHighscorePage = (): JSX.Element => {
     const difficulty = javazone2.enabled ? 'Javazone1' : 'Javazone42'
     const { data: players } = useSWR(
         '/players',
-        () => getByDifficulty(difficulty, 200),
+        () => getByDifficulty('Javazone42', 200),
         { refreshInterval: 1000 * 10 },
     )
     if (players === undefined) {
