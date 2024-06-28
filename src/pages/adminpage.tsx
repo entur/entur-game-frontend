120b2f2f74dca64801a5098cb3f0f3931819cb98
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-06-28 15:51:32 +0200

    added AdminNavBar and navigation cards

--- src/pages/adminpage.tsx
+++ src/pages/adminpage.tsx
@@ -2,10 +2,11 @@ import { ReactElement, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TextField } from '@entur/form';
 import { SuccessButton } from '@entur/button';
-import { Heading1 } from '@entur/typography';
-import { ForwardIcon } from '@entur/icons';
-import { Contrast } from '@entur/layout';
+import { Heading2, LeadParagraph,  } from '@entur/typography';
+import { AdjustmentsIcon, BulletListIcon } from '@entur/icons';
+import { Contrast, NavigationCard } from '@entur/layout';
 import { useBackground } from '../contexts/backgroundContext'
+import { AdminNavBar } from '@/components/NavBar/AdminNavBar';
 
 export function AdminPage(): ReactElement {
     const { setBackgroundColor } = useBackground()
@@ -18,18 +19,32 @@ export function AdminPage(): ReactElement {
     // må legge inn funksjonelitet for å hente fra API
     const navigate = useNavigate();
 
+    // tailwing og styling på endres på
     return (
-        <div className='max-w-md mx-auto p-4'>
-
-                <Heading1>
-                    Admin Panel
-                </Heading1>
-                <div className="space-y-10 mt-10">
-                    <TextField size="medium" label="Start" variant="information"></TextField>
-                    <TextField size="medium" label="Stopp" variant="information"></TextField>
-                    <SuccessButton onClick={() => navigate('/')}> <ForwardIcon /></SuccessButton> 
-                </div>
-             
+        <div>
+            <AdminNavBar></AdminNavBar>
+            <div className='flex items-center justify-center'>
+                <Heading2>
+                    Spillets admin-panel
+                </Heading2>
+            </div>
+            <div className='flex items-center justify-center'>
+                <LeadParagraph>Konfigurer nye ruter og se nåværende leaderboard</LeadParagraph>
+            </div>
+            <div className='flex flex-row items center'> 
+                <NavigationCard 
+                    title="Opprett rute" 
+                    titleIcon={<AdjustmentsIcon/>} 
+                    href="https://om.entur.no/reisende/reiseplanleggeren/">
+                        Her oppretter du en ny rute.  Du velger hvor og når ruten begynner, og hvor den skal ende. 
+                </NavigationCard>
+                <NavigationCard 
+                    title="Leaderboard" 
+                    titleIcon={<BulletListIcon/>}  
+                    href="https://om.entur.no/reisende/reiseplanleggeren/">
+                        Her har du oversikt over alle lagrede poengsummer og mulighet til å trekke en vinner blandt de beste poengsummene. 
+                </NavigationCard>
+            </div>
         </div>
     );
 }
