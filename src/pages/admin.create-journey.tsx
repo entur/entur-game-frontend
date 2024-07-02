fed6ad1f23e592d57c08db1e9b952f5dc79c677e
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-07-02 08:43:52 +0200

    added timepicker and searchable dropdown

--- src/pages/admin.create-journey.tsx
+++ src/pages/admin.create-journey.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TextField } from '@entur/form';
 import { SuccessButton } from '@entur/button';
@@ -6,6 +6,9 @@ import { Heading1, Heading3, LeadParagraph } from '@entur/typography';
 import { ForwardIcon, MapPinIcon, DestinationIcon } from '@entur/icons';
 import { useBackground } from '../contexts/backgroundContext'
 import { BlockquoteFooter } from '@entur/typography';
+import { TimePicker } from '@entur/datepicker';
+import { now, ZonedDateTime } from '@internationalized/date';
+import { SearchableDropdown } from '@entur/dropdown';
 
 export function AdminCreateJourney(): ReactElement {
     const { setBackgroundColor } = useBackground()
@@ -15,8 +18,9 @@ export function AdminCreateJourney(): ReactElement {
         return () => setBackgroundColor('$colors-brand-white')
     }, [setBackgroundColor])
 
-    // må legge inn funksjonelitet for å hente fra API
+    const [selected, setSelected] = useState(null)
     const navigate = useNavigate();
+    const [time, setTime] = useState<ZonedDateTime | null>(now('Europe/Oslo'))
 
     return (
         <div className='max-w-md mx-auto p-4'>
@@ -27,12 +31,21 @@ export function AdminCreateJourney(): ReactElement {
                 <LeadParagraph>Konfigurer ny rute ved å angi start, mål og starttidspunkt</LeadParagraph>
                 <div className="space-y-10 mt-10">
                     <Heading3>Velg start og mål</Heading3>
-                    <TextField size="medium" label="Start" variant="information" prepend=<MapPinIcon/>> </TextField>
-                    <TextField size="medium" label="Mål" variant="information" prepend=<DestinationIcon/>> </TextField>
+                    <SearchableDropdown
+                        label="Start"
+                        items={cities}
+                        selectedItem={selected}
+                        onChange={setSelected}
+                        />
+                    <TextField size="medium" label="Mål" variant="information"></TextField>
                     <SuccessButton onClick={() => navigate('/')}> <ForwardIcon /></SuccessButton> 
                 </div>
                 <div className="space-y-10 mt-10">
                     <Heading3>Velg starttidspunkt</Heading3>
+                    <TimePicker
+                        label="Tid"
+                        selectedTime={time}
+                        onChange={(time: ZonedDateTime | null)=> setTime(time)}/>
                 </div>
         </div>
     );
