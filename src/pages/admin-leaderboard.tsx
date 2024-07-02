faf6c28af3bf9e73e59fe9aaad2976f9916f897a
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-07-02 15:01:06 +0200

    added empty leaderboard page with navbar

---
+++ src/pages/admin-leaderboard.tsx
@@ -0,0 +1,6 @@
+import { AdminNavBar } from '@/components/NavBar/AdminNavBar'
+import { ReactElement } from 'react'
+
+export function LeaderboardPage(): ReactElement {
+    return <AdminNavBar></AdminNavBar>
+}
