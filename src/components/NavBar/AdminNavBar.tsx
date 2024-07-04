5ad3cca259cc0f807bbea7e45c5428f647646ea4
Magnus Rosvold Farstad <magnus.r.farstad@outlook.com>
2024-07-04 16:11:40 +0200

    added active prop to admin navbar items

--- src/components/NavBar/AdminNavBar.tsx
+++ src/components/NavBar/AdminNavBar.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import { TopNavigationItem } from '@entur/menu'
+import { usePathname } from 'next/navigation'
 
 export const AdminNavBar: React.FC = () => {
+    const path = usePathname()
     return (
         <nav className="flex flex-row items-center gap-14">
-            <TopNavigationItem href="/admin/create-journey">
+            <TopNavigationItem
+                active={path.endsWith('/create-journey')}
+                href="/admin/create-journey"
+            >
                 Opprett rute
             </TopNavigationItem>
-            <TopNavigationItem href="/admin/leaderboard">
+            <TopNavigationItem
+                active={path.endsWith('/leaderboard')}
+                href="/admin/leaderboard"
+            >
                 Leaderboard
             </TopNavigationItem>
         </nav>
