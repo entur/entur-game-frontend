48ce4e25a3a981c15161ea4ea6951de02e9b8cc6
Oscar Aleksander Halvorsen <oscar@bekk-mac-2281.shared>
2024-07-08 13:25:26 +0200

    in leaderboard get all the data from db

--- src/app/(white)/admin/leaderboard/[eventName]/page.tsx
+++ src/app/(white)/admin/leaderboard/[eventName]/page.tsx
@@ -6,37 +6,35 @@ import { useParams } from 'next/navigation'
 import { BlockquoteFooter, Heading1, LeadParagraph } from '@entur/typography'
 import { Loader } from '@entur/loader'
 
-import { getEventByEventName } from '@/lib/api/eventApi'
-import { Event, PlayerScore} from '@/lib/types/types'
+import { PlayerScore } from '@/lib/types/types'
 import { DataCell, HeaderCell, Table, TableBody, TableHead, TableRow } from '@entur/table';
 import { getPlayerScoresByActiveEvent } from '@/lib/api/playerScoreApi'
 
 export default function GamePage(): JSX.Element {
 
-     // const { eventName } : {eventName: string} = useParams() TODO: kanskje behold
+    // const { eventName } : {eventName: string} = useParams() TODO: kanskje behold
 
-    const [score, setScore] = useState<PlayerScore | null>(null) 
+    const [scores, setScores] = useState<PlayerScore[]>([]) 
     const [isScoreError, setScoreError] = useState<boolean>(false)
 
     useEffect(() => {
-        async function getScore() {
-            const score = await getPlayerScoresByActiveEvent()
-            console.log("print score")
-            console.log(score)
-            if (score === null) {
+        async function getScores() {
+            const scores = await getPlayerScoresByActiveEvent()
+            console.log("print scores")
+            console.log(scores)
+            if (scores === null || scores.length === 0) {
                 setScoreError(true)
                 return
             } else {
                 setScoreError(false)
-                setScore(score[0])
+                setScores(scores)
             }
-            
         }
-        getScore()
+        getScores()
     }, [])
 
 
-    if (isScoreError || score === null) {
+    if (isScoreError || scores.length === 0 || scores == null) {
         // TODO: redirect to main screen
         return (
             <div className="max-w-screen-xl xl:ml-72 xl:mr-40 ml-10 mr-10">
@@ -55,30 +53,22 @@ export default function GamePage(): JSX.Element {
                 </LeadParagraph>
             </div>
             <Table>
-            <TableHead>
-                <TableRow>
-                <HeaderCell>Spiller</HeaderCell>
-                <HeaderCell>Reisetid</HeaderCell>
-                <HeaderCell>Poengsum</HeaderCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                <TableRow>
-                <DataCell>{score.player.playerName}</DataCell>
-                <DataCell>{score.totalTravelTime}</DataCell>
-                <DataCell>{score.scoreValue}</DataCell>
-                </TableRow>
-                <TableRow>
-                <DataCell>Tomas</DataCell>
-                <DataCell>11.00</DataCell>
-                <DataCell>Østerås</DataCell>
-                </TableRow>
-                <TableRow>
-                <DataCell>Selma</DataCell>
-                <DataCell>12.00</DataCell>
-                <DataCell>Østerås</DataCell>
-                </TableRow>
-            </TableBody>
+                <TableHead>
+                    <TableRow>
+                        <HeaderCell>Spiller</HeaderCell>
+                        <HeaderCell>Reisetid</HeaderCell>
+                        <HeaderCell>Poengsum</HeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {scores.map((score, index) => (
+                        <TableRow key={index}>
+                            <DataCell>{score.player.playerName}</DataCell>
+                            <DataCell>{score.totalTravelTime}</DataCell>
+                            <DataCell>{score.scoreValue}</DataCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
             </Table>
         </div>
     )
