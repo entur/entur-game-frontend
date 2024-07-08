0220777a069d25254d24bc6f49ac78508a969557
Oscar Aleksander Halvorsen <oscar@bekk-mac-2281.shared>
2024-07-08 12:22:54 +0200

    flyttet leaderboard/eventname til white/admin

--- src/app/(white)/admin/leaderboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactElement } from 'react'
-import { AdminNavBar } from '@/components/NavBar/AdminNavBar'
-
-export default function AdminLeaderboardPage(): ReactElement {
-    return <AdminNavBar />
-}
