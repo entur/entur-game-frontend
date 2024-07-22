706cd441a4c2c5ef648c6aa5d18cd292f23cb329
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-07-22 13:09:21 +0200

    changed heigh and width to baseCard component

--- src/app/(white)/admin/components/InactiveEventsList.tsx
+++ src/app/(white)/admin/components/InactiveEventsList.tsx
@@ -13,25 +13,25 @@ const InactiveEventsList: React.FC = (): JSX.Element => {
         return <div>Error: {error}</div>
     }
 
-    const handleDeleteEvent = async (eventId: number | undefined) => {
-        try {
-            await deleteEvent(eventId)
-            setEvents(events.filter((event) => event.eventId !== eventId)) // Remove deleted event from state
-        } catch (error) {
-            console.error('Failed to delete event:', error)
-        }
-    }
+    // const handleDeleteEvent = async (eventId: number | undefined) => {
+    //     try {
+    //         await deleteEvent(eventId)
+    //         setEvents(events.filter((event) => event.eventId !== eventId)) // Remove deleted event from state
+    //     } catch (error) {
+    //         console.error('Failed to delete event:', error)
+    //     }
+    // }
 
     return (
         <div>
             {events.map((event) => (
-                <div className="flex pb-4 gap-4 align-baseline">
+                <div className="flex pb-4 gap-4  align-baseline">
                     <BaseCard
                         key={event.eventId}
-                        className="mb-4 max-w-lg max-h-20"
+                        className="mb-4 max-h-20 flex-1 min-w-[200px] max-w-[480px] h-[80px] p-4"
                     >
                         <TravelHeader
-                            className="p-4"
+                            className="max-w-full"
                             size="large"
                             from={event.startLocationName}
                             to={event.endLocationNames[0] || ''}
@@ -41,7 +41,7 @@ const InactiveEventsList: React.FC = (): JSX.Element => {
                     <Button
                         variant="negative"
                         size="small"
-                        onClick={() => handleDeleteEvent(event.eventId)}
+                        // onClick={() => handleDeleteEvent(event.eventId)}
                     >
                         <DeleteIcon className="inline align-baseline" />
                         Slett spill
