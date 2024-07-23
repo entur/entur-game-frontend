9000e526495a5e72d001239bf5bb2aa1903df4e1
Marianne Stålen <98464130+maribsta@users.noreply.github.com>
2024-07-23 13:13:21 +0200

    Add map (#42)
    * add map without styling
    
    * delete unused imports
    
    * added grid to map
    
    * Add marker
    
    * Add posistion to pins
    
    * import
    
    * fix things after merge
    
    * style map and add current position
    
    * Fix Magnus comments
    
    * change destination svg

--- src/app/(dark)/game/[eventName]/page.tsx
+++ src/app/(dark)/game/[eventName]/page.tsx
@@ -1,14 +1,18 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'next/navigation'
-import { Heading1 } from '@entur/typography'
+import { Heading1, Heading3 } from '@entur/typography'
 import { Loader } from '@entur/loader'
 import Game from '@/components/Game/GameScreen'
 import GameNavBar from '@/components/NavBar/GameNavBar'
 import { getEventByEventName, Result } from '@/lib/api/eventApi'
-import { Event } from '@/lib/types/types'
+import { Event, StopPlace } from '@/lib/types/types'
+import { GridContainer, GridItem } from '@entur/grid'
 import useSWR from 'swr'
+import Map from '../components/Map'
+import { MapPinIcon, DestinationIcon, StandingIcon } from '@entur/icons'
+import { Contrast } from '@entur/layout'
 
 export default function GamePage(): JSX.Element {
     const [startTimer] = useState<number>(Date.now())
@@ -29,6 +33,14 @@ export default function GamePage(): JSX.Element {
         ? Math.ceil((3 * event.optimalTravelTime) / (60 * 60)) * 1000 * 60 * 60
         : null
 
+    const [startLocation, setStartLocation] = useState<StopPlace | undefined>()
+
+    useEffect(() => {
+        if (event?.startLocation) {
+            setStartLocation(event.startLocation)
+        }
+    }, [event])
+
     return (
         <>
             {isLoading ? (
@@ -38,7 +50,8 @@ export default function GamePage(): JSX.Element {
                     <Heading1>Spill ikke funnet</Heading1>
                 </div>
             ) : (
-                event && (
+                event &&
+                startLocation && (
                     <main className="flex flex-col">
                         <div className="sm:sticky top-20">
                             <GameNavBar
@@ -47,18 +60,53 @@ export default function GamePage(): JSX.Element {
                                 maxTime={maxTime}
                             />
                         </div>
-                        <div className="max-w-screen-xl xl:ml-72 xl:mr-40 ml-10 mr-10">
-                            <Game
-                                name={''}
-                                event={event}
-                                startTimer={startTimer}
-                                // eslint-disable-next-line @typescript-eslint/no-empty-function
-                                handleWinner={() => {}}
-                                maxTime={maxTime}
-                                setUsedTime={setUsedTime}
-                                numLegs={numLegs}
-                                setNumLegs={setNumLegs}
-                            />
+                        <div className="max-w-screen-2xl xl:ml-72 xl:mr-40 ml-10 mr-10">
+                            <GridContainer spacing="large">
+                                <GridItem small={7} className="grid-demo-item">
+                                    <Game
+                                        name={''}
+                                        event={event}
+                                        startTimer={startTimer}
+                                        // eslint-disable-next-line @typescript-eslint/no-empty-function
+                                        handleWinner={() => {}}
+                                        maxTime={maxTime}
+                                        setUsedTime={setUsedTime}
+                                        numLegs={numLegs}
+                                        setNumLegs={setNumLegs}
+                                        startLocation={startLocation}
+                                        setStartLocation={setStartLocation}
+                                    />
+                                </GridItem>
+
+                                <GridItem small={5} className="grid-demo-item">
+                                    <Contrast>
+                                        <Map
+                                            event={event}
+                                            currentPosition={startLocation}
+                                        />
+                                        <div className="icon-container">
+                                            <div className="icon-item">
+                                                <MapPinIcon className="text-coral" />
+                                                <Heading3 className="map-text">
+                                                    Start
+                                                </Heading3>
+                                            </div>
+                                            <div className="icon-item">
+                                                <DestinationIcon className="text-coral" />
+                                                <Heading3 className="map-text">
+                                                    Mål
+                                                </Heading3>
+                                            </div>
+                                            <div className="icon-item">
+                                                <StandingIcon className="text-coral" />
+                                                <Heading3 className="map-text">
+                                                    Din posisjon
+                                                </Heading3>
+                                            </div>
+                                        </div>
+                                    </Contrast>
+                                </GridItem>
+                            </GridContainer>
                         </div>
                     </main>
                 )
