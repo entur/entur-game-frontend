e6c797f64ccd680292cbbc9a14cf2424b32b7c8b
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-07-23 12:49:50 +0200

    added banner instead of div

--- src/app/(white)/admin/components/InactiveEventsList.tsx
+++ src/app/(white)/admin/components/InactiveEventsList.tsx
@@ -1,26 +1,24 @@
 'use client'
 import { getScoresEventId } from '@/lib/api/scoreApi'
 import { useInactiveStopPlaces } from '@/lib/hooks/useInactiveStopPlaceName'
+import { BannerAlertBox } from '@entur/alert'
 import { Button } from '@entur/button'
 import { DeleteIcon } from '@entur/icons'
 import { BaseCard } from '@entur/layout'
 import { TravelHeader } from '@entur/travel'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
 
 const InactiveEventsList: React.FC = (): JSX.Element => {
     const { events, error } = useInactiveStopPlaces()
     const router = useRouter()
 
     if (error) {
-        return <div>Error: {error}</div>
+        return (
+            <BannerAlertBox title="Stoppesteder fins ikke" variant="negative">
+                {error}
+            </BannerAlertBox>
+        )
     }
-
-    useEffect(() => {
-        if (!router) {
-            console.error('Router instance not found')
-        }
-    }, [router])
 
     const handleNavigateLeaderboard = (eventId: number | undefined) => {
         if (router && eventId !== undefined) {
@@ -32,11 +30,11 @@ const InactiveEventsList: React.FC = (): JSX.Element => {
     return (
         <div>
             {events.map((event) => (
-                <div className="flex pb-4 gap-4 align-baseline">
-                    <BaseCard
-                        key={event.eventId}
-                        className="mb-4 max-h-20 flex-1 min-w-[200px] max-w-[480px] h-[80px] p-4 cursor-pointer hover:bg-gray-200 transform hover:-translate-y-0.5 transition-all duration-200 hover:shadow-lg"
-                    >
+                <div
+                    key={event.eventId}
+                    className="flex pb-4 gap-4 align-baseline"
+                >
+                    <BaseCard className="mb-4 max-h-20 flex-1 min-w-[200px] max-w-[480px] h-[80px] p-4 cursor-pointer hover:bg-gray-200 transform hover:-translate-y-0.5 transition-all duration-200 hover:shadow-lg">
                         <TravelHeader
                             className="max-w-full"
                             size="large"
