f8265f9cdabb7381d93127dba9627832b9fa3c5a
Oscar Aleksander Halvorsen <oscar@bekk-mac-2281.shared>
2024-07-24 07:53:44 +0200

    laget enkel ScoreScreen

--- src/components/Game/VictoryScreen/ScoreScreen.tsx
+++ src/components/Game/VictoryScreen/ScoreScreen.tsx
@@ -1,28 +1,94 @@
 'use client'
 
-import { Heading1, Paragraph } from '@entur/typography'
-import { PrimaryButton } from '@entur/button'
+import { Heading1, Heading3 } from '@entur/typography'
 import React from 'react'
 import { Contrast } from '@entur/layout'
 import { Event } from '@/lib/types/types'
+import {
+    DataCell,
+    HeaderCell,
+    Table,
+    TableBody,
+    TableHead,
+    TableRow,
+} from '@entur/table'
+
 interface ScoreScreenProps {
     event: Event
+    numLegs: number
 }
 
-function ScoreScreen({ event }: ScoreScreenProps): JSX.Element {
+function ScoreScreen({ event, numLegs }: ScoreScreenProps): JSX.Element {
     window.scrollTo(0, 0)
     return (
-        <>
-            <Contrast>
-                <Heading1>Resultater</Heading1>
-                <Paragraph>
-                    {`Du rakk dessverre ikke å komme deg til ${event.eventName} i tide.`}
-                </Paragraph>
-                <PrimaryButton onClick={() => window.location.reload()}>
-                    Prøv igjen
-                </PrimaryButton>
-            </Contrast>
-        </>
+        <Contrast className="text-center">
+            <Heading1>Resultater</Heading1>
+            <div className="relative w-full mt-8">
+                <Heading1 className="absolute inset-x-0 right-12 top-28 text-6xl text-coral transform rotate-6">
+                    75
+                </Heading1>
+                <Heading1 className="absolute inset-x-0 right-12 top-40 text-4xl text-coral transform rotate-6">
+                    poeng
+                </Heading1>
+            </div>
+            <div>
+                <Heading3>Oppsummering</Heading3>
+                <div className="min-h-screen min-w-screen flex justify-center">
+                    <div className="w-1/2">
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <HeaderCell className="text-left">
+                                        {' '}
+                                    </HeaderCell>
+                                    <HeaderCell className="text-left">
+                                        Deg
+                                    </HeaderCell>
+                                    <HeaderCell className="text-left">
+                                        Reiseplanleggeren
+                                    </HeaderCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <DataCell className="text-left">
+                                        Reisetid
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        Null
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        {event.optimalTravelTime}
+                                    </DataCell>
+                                </TableRow>
+                                <TableRow>
+                                    <DataCell className="text-left">
+                                        Reiselengde
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        Null
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        Null
+                                    </DataCell>
+                                </TableRow>
+                                <TableRow>
+                                    <DataCell className="text-left">
+                                        Antall steg
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        {numLegs}
+                                    </DataCell>
+                                    <DataCell className="text-left">
+                                        {event.optimalStepNumber}
+                                    </DataCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
+            </div>
+        </Contrast>
     )
 }
 
