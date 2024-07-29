e03f12462189e60a6d5055f7b9d141ac177fb0f7
Selma Gudmundsen <selma.gudmundsen@gmail.com>
2024-07-29 12:45:25 +0200

    added styling of components

--- src/components/Game/DeadScreen.tsx
+++ src/components/Game/DeadScreen.tsx
@@ -1,37 +1,49 @@
 'use client'
 
-import { Heading2, Heading3, Paragraph } from '@entur/typography'
+import { Heading1, Heading2, Heading3 } from '@entur/typography'
 import { PrimaryButton, SecondaryButton } from '@entur/button'
 import React from 'react'
 import { Contrast } from '@entur/layout'
 import Ripped_Ticket from '@/lib/assets/images/Ripped_Ticket.svg'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 function DeadScreen(): JSX.Element {
+    const router = useRouter()
     window.scrollTo(0, 0)
+
+    const handleNavigation = () => {
+        router.push('/')
+    }
+
     return (
         <>
             <Contrast>
-                <div className="flex flex-col items-center">
-                    <Heading2>Ånei! Billetten din er utløpt!</Heading2>
-                    <Paragraph>
+                <div className="flex flex-col items-center w-full">
+                    <Heading1
+                        className="text-coral mb-10 text-55"
+                        margin="none"
+                    >
+                        Ånei! Billetten din er utløpt!
+                    </Heading1>
+                    <Heading2 margin="none">
                         Du rakk dessverre ikke fram til mål i tide
-                    </Paragraph>
-
+                    </Heading2>
                     <Image
+                        className="mb-16 mt-10"
                         src={Ripped_Ticket}
                         alt="entur partner"
-                        width={600}
+                        width={500}
                         style={{ height: 'auto' }}
                         priority={true}
                     />
-                    <Heading3 margin="both">
+                    <Heading3 margin="none">
                         Velg om du vil avslutte eller prøve igjen
                     </Heading3>
-                    <div className="flex gap-6 mt-10">
+                    <div className="flex gap-6 mt-6">
                         <SecondaryButton
                             size="large"
-                            onClick={() => window.location.reload()}
+                            onClick={handleNavigation}
                         >
                             Avslutt
                         </SecondaryButton>
