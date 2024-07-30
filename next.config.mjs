2deabc1ee2bccbbf5d02593b4ecd0080a2ecc732
Magnus Rosvold Farstad <magnus.r.farstad@outlook.com>
2024-07-30 16:52:21 +0200

    add proxy rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   compiler: {
     removeConsole: false,
   },
+  async rewrites() {
+    console.log('process.env.apiUrl', process.env.apiUrl)
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `/api/auth/:path*`,
+      },
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
