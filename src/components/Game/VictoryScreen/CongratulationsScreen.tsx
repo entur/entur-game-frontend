b6f80c505a6cbb6bf46bea6304e1da1b15038df6
Oscar Aleksander Halvorsen <oscar@bekk-mac-2281.shared>
2024-07-31 10:51:29 +0200

    fikset congratulations-page

--- src/components/Game/VictoryScreen/CongratulationsScreen.tsx
+++ src/components/Game/VictoryScreen/CongratulationsScreen.tsx
@@ -1,11 +1,10 @@
-'use client'
-
 import { Heading1 } from '@entur/typography'
 import { PrimaryButton } from '@entur/button'
 import React from 'react'
 import { Contrast } from '@entur/layout'
 import { ForwardIcon } from '@entur/icons'
-import Gratulerer from '@/lib/assets/images/Gratulerer.svg'
+import Framme_background from '@/lib/assets/images/Framme_Background.svg'
+import Framme_Ticket from '@/lib/assets/images/Framme_Ticket.svg'
 import Image from 'next/image'
 import { Screen } from './VictoryScreen'
 
@@ -23,14 +22,22 @@ function CongratulationsScreen({
         <Contrast className="text-center">
             <Heading1>Gratulerer, du er fremme!</Heading1>
             <div className="relative w-full mt-8">
-                <Heading1 className="absolute inset-x-0 right-12 2xl:right-16 top-28 xl:top-36 2xl:top-44 text-6xl xl:text-7xl 2xl:text-8xl text-coral transform rotate-6">
-                    {scoreValue}
-                </Heading1>
-                <Heading1 className="absolute inset-x-0 right-16 2xl:right-20 top-40 xl:top-52 2xl:top-64 text-5xl 2xl:text-6xl text-coral transform rotate-6">
-                    poeng
-                </Heading1>
+                <div className="relative">
+                    <Image
+                        src={Framme_Ticket}
+                        alt="entur partner"
+                        width={360}
+                        className="absolute inset-x-0 mx-auto -top-4 xl:top-8 2xl:top-20"
+                    />
+                    <Heading1 className="absolute inset-x-0 right-16 top-20 xl:top-32 2xl:top-44 text-8xl text-coral transform rotate-6">
+                        {scoreValue}
+                    </Heading1>
+                    <Heading1 className="absolute inset-x-0 right-20 top-40 xl:top-52 2xl:top-64 text-6xl text-coral transform rotate-6">
+                        poeng
+                    </Heading1>
+                </div>
                 <Image
-                    src={Gratulerer}
+                    src={Framme_background}
                     alt="entur partner"
                     width={2000}
                     className="my-4"
